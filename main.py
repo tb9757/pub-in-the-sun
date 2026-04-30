7893ae790564ea0e9f229026a6d713ea94e08df8
tb9757 <tb9757@gmail.com>
2026-04-30 11:51:05 +0100

    Add logic to extract pub names

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 import json
-import requests
+import httpx
 import os
+
+app = FastAPI(title="Pub in the Sun")
+load_dotenv()
+
+HERE_API = os.getenv('HERE_API_KEY')
+BASE_URL = "https://discover.search.hereapi.com/v1/discover?q=pub&limit=20"
+
+
+
+@app.get("/")
+async def root(lat, lng):
+    pub_names = []
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{BASE_URL}&in=circle:{lat},{lng};r=1000&apiKey={HERE_API}")
+    for item in response.json()['items']:
+        pub_names.append(item['title'])
+    return pub_names
